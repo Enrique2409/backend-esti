a52f8dfda18e62e92118028feb894299e1d71d41
Enrique2409 <e.kikerade@gmail.com>
2024-12-13 20:12:00 -0600

    create package

---
+++ src/test/java/org/esti/backend_esti/BackendEstiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esti.backend_esti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEstiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
